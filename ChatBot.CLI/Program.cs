709b7ef03d60c5d359574ff07a087e5d95ad8f91
Nick Larsen <fodylarsen@gmail.com>
2019-10-11 12:06:32 -0400

    save stream notes to drafts folder, not posts folder

--- ChatBot.CLI/Program.cs
+++ ChatBot.CLI/Program.cs
@@ -25,7 +25,7 @@ namespace ChatBot.CLI
             twitchBot.Initialize(twitchUsername, twitchAccessToken, twitchApiClientId, twitchApiAccessToken, channel, teamUrlSlug);
             twitchBot.ConnectChat();
 
-            string templateFilename = $@"I:\culture-of-development\culture-of-development.github.com\source\_posts\s0000-{DateTime.UtcNow.ToString("yyyyMMdd")}.md";
+            string templateFilename = $@"I:\culture-of-development\culture-of-development.github.com\source\_drafts\s0000-{DateTime.UtcNow.ToString("yyyyMMdd")}.md";
             outputStreamTemplateTimer = new Timer(60_000);
             outputStreamTemplateTimer.Elapsed += (o, _) => twitchBot.WriteMarkdownTemplate(templateFilename);
             outputStreamTemplateTimer.AutoReset = true;
